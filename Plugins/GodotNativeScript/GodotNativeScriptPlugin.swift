71e2329ac6bcbd0807b18b6787a814d0be2d34fa
Kyle <kyle201817146@gmail.com>
2022-04-03 10:21:14 +0800

    Update plugin

--- Plugins/GodotNativeScript/GodotNativeScriptPlugin.swift
+++ Plugins/GodotNativeScript/GodotNativeScriptPlugin.swift
@@ -2,22 +2,20 @@ import PackagePlugin
 
 @main struct GodotNativeScriptPlugin: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
-        let tool = try context.tool(named: "GodotNativeScriptGenerator")
-        let directory = context.pluginWorkDirectory
-//        let sources: [Path] = context.inputFiles.filter { $0.type == .source }.map(\.path)
-        
-        print("Hello")
-        print(directory.string)
-        
-        let sources: [Path] = []
+        guard let target = target as? SourceModuleTarget else {
+            return []
+        }
 
+        let tool = try context.tool(named: "GodotNativeScriptGenerator")
+        let sources: [Path] = target.sourceFiles.filter { $0.type == .source }.map(\.path)
+
+        let directory = context.pluginWorkDirectory
         let output: (staged: Path, common: Path, classes: Path) =
             (
                 directory.appending("registration.swift"),
                 directory.appending("common.swift"),
                 directory.appending("classes.swift")
             )
-
         return [
             .buildCommand(
                 displayName: "Generating files '\(output.common)', '\(output.classes)'",
